2e9513827ec3783f32e35d74040ade3d2e8a6d5a
Vaskold <ladius23@gmail.com>
2018-03-02 00:50:33 +0300

    'little_adjust_to_task'

--- all.py
+++ all.py
@@ -1,14 +1,9 @@
 import xml.dom.minidom as md
-import sqlite3
 
 
-from tasks import xml2ram, ram2xml, ram2sqlite, ram
+from tasks import xml2ram, ram2xml
 xml = md.parse("tasks.xml")
 schema = xml2ram(xml)
 resXML = ram2xml(schema)
 with open("result.xml", "w") as file:
     file.write(resXML.toprettyxml(encoding="utf-8").decode("utf-8"))
-connect = sqlite3.connect(":memory:")  # используем оперативную память
-ram2sqlite(schema, connect)
-connect.close()
-del connect
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,4 @@
 from tasks.xml2ram import xml2ram
 from tasks.ram2xml import ram2xml
-from tasks.ram2sqlite import ram2sqlite
 from tasks.ram import Constraint, Index, IndexItem, Domain, Field, Schema, Table
 __all__ = ("xml2ram", "ram2xml", "ram2sqlite")
